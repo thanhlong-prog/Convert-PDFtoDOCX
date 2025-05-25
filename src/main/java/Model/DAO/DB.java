aa46c366bbe9467552bbb25482bbe6a58155a796
hydric11x6 <minhquangtran1106@gmail.com>
2025-05-25 16:13:12 +0700

    quang ngu

--- src/main/java/Model/DAO/DB.java
+++ src/main/java/Model/DAO/DB.java
@@ -6,6 +6,6 @@ import java.sql.DriverManager;
 public class DB {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/convert_pdf2docx", "root", "123456");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/convert_pdf2docx", "root", "");
     }
 }
